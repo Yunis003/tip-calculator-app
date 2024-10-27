5fb0d55ee327a2ab3a416083e1d9bd846fd202fd
Yunis003 <hi.yunisqulamov@gmail.com>
2024-10-27 13:37:28 +0400

    final

---
+++ README.md
@@ -0,0 +1,17 @@
+### Tip Calculator
+A simple, responsive Tip Calculator app built using HTML, CSS, and JavaScript. This app allows users to calculate tips easily by entering the bill amount, selecting a tip percentage, and optionally splitting the bill among multiple people.
+
+## Features
+Tip Calculation: Calculate tips based on the bill amount and desired tip percentage.
+Bill Splitting: Split the total amount (bill + tip) among multiple people.
+
+## Technologies Used
+HTML: Structure of the application.
+CSS: Styling and layout.
+JavaScript: Logic for calculations and user interactions.
+
+# Installation
+Copy code
+git clone https://github.com/yourusername/tip-calculator.git
+
+Or you can just click the (Live Preview Below):
--- assets/app.js
+++ assets/app.js
@@ -23,16 +23,279 @@ sendBtn.addEventListener("click", ()=>{
     let priceTip = document.querySelector(".priceTip");
     let priceTotal = document.querySelector(".priceTotal");
 
-    if (obj.customPercentage != null){
-        let anotherRes = parseInt(bill * (obj.customPercentage * people)) / 100;
-        priceTip.textContent = anotherRes + "$";
-        priceTotal.textContent = parseInt(anotherRes) + parseInt(bill) + "$";    
-    }
-    else{
+    
     let result = parseInt(bill * (obj.tipPercentage * people)) / 100; 
     priceTip.textContent = result + "$";
     priceTotal.textContent = parseInt(result) + parseInt(bill) + "$";
-    }
 });
 
 
+// let sendBtn = document.querySelector(".submit");
+// let customPercentag = document.querySelector(".customPercentage").value;
+// let obj = {
+//     tipPercentage: null,
+//     customPercentage: customPercentag
+// };
+
+
+
+
+
+
+// let tips = document.querySelectorAll(".tip");
+// console.log(tips);
+// tips.forEach((tip)=>{
+//     tip.addEventListener("click", ()=>{
+//         document.querySelector(".salam").classList.remove("salam");
+//         tip.classList.add("salam");
+
+//         let percentage = tip.querySelector(".percentage").textContent;
+//         obj.tipPercentage = percentage;
+//     })
+// })
+
+// sendBtn.addEventListener("click", ()=>{
+//     let bill = document.querySelector(".billInput").value;
+//     let people = document.querySelector(".peopleInput").value;
+//     let priceTip = document.querySelector(".priceTip");
+//     let priceTotal = document.querySelector(".priceTotal");
+
+//     if (obj.customPercentage){
+//         let anotherRes = parseInt(bill * (obj.customPercentage * people)) / 100;
+//         priceTip.textContent = anotherRes + "$";
+//         priceTotal.textContent = parseInt(anotherRes) + parseInt(bill) + "$";    
+//     }
+//     else{
+//     let result = parseInt(bill * (obj.tipPercentage * people)) / 100; 
+//     priceTip.textContent = result + "$";
+//     priceTotal.textContent = parseInt(result) + parseInt(bill) + "$";
+//     }
+// });
+// let sendBtn = document.querySelector(".submit");
+// let customPercentag = document.querySelector(".customPercentage").value;
+// let obj = {
+//     tipPercentage: null,
+//     customPercentage: customPercentag
+// };
+
+// let tips = document.querySelectorAll(".tip");
+// console.log(tips);
+// tips.forEach((tip)=>{
+//     tip.addEventListener("click", ()=>{
+//         document.querySelector(".salam").classList.remove("salam");
+//         tip.classList.add("salam");
+
+//         let percentage = tip.querySelector(".percentage").textContent;
+//         obj.tipPercentage = percentage;
+//     })
+// })
+
+// sendBtn.addEventListener("click", ()=>{
+//     let bill = document.querySelector(".billInput").value;
+//     let people = document.querySelector(".peopleInput").value;
+//     let priceTip = document.querySelector(".priceTip");
+//     let priceTotal = document.querySelector(".priceTotal");
+
+//     if (obj.customPercentage){
+//         let anotherRes = parseInt(bill * (obj.customPercentage * people)) / 100;
+//         priceTip.textContent = anotherRes + "$";
+//         priceTotal.textContent = parseInt(anotherRes) + parseInt(bill) + "$";    
+//     }
+//     else{
+//     let result = parseInt(bill * (obj.tipPercentage * people)) / 100; 
+//     priceTip.textContent = result + "$";
+//     priceTotal.textContent = parseInt(result) + parseInt(bill) + "$";
+//     }
+// });
+// let sendBtn = document.querySelector(".submit");
+// let customPercentag = document.querySelector(".customPercentage").value;
+// let obj = {
+//     tipPercentage: null,
+//     customPercentage: customPercentag
+// };
+
+// let tips = document.querySelectorAll(".tip");
+// console.log(tips);
+// tips.forEach((tip)=>{
+//     tip.addEventListener("click", ()=>{
+//         document.querySelector(".salam").classList.remove("salam");
+//         tip.classList.add("salam");
+
+//         let percentage = tip.querySelector(".percentage").textContent;
+//         obj.tipPercentage = percentage;
+//     })
+// })
+
+// sendBtn.addEventListener("click", ()=>{
+//     let bill = document.querySelector(".billInput").value;
+//     let people = document.querySelector(".peopleInput").value;
+//     let priceTip = document.querySelector(".priceTip");
+//     let priceTotal = document.querySelector(".priceTotal");
+
+//     if (obj.customPercentage){
+//         let anotherRes = parseInt(bill * (obj.customPercentage * people)) / 100;
+//         priceTip.textContent = anotherRes + "$";
+//         priceTotal.textContent = parseInt(anotherRes) + parseInt(bill) + "$";    
+//     }
+//     else{
+//     let result = parseInt(bill * (obj.tipPercentage * people)) / 100; 
+//     priceTip.textContent = result + "$";
+//     priceTotal.textContent = parseInt(result) + parseInt(bill) + "$";
+//     }
+// });
+// let sendBtn = document.querySelector(".submit");
+// let customPercentag = document.querySelector(".customPercentage").value;
+// let obj = {
+//     tipPercentage: null,
+//     customPercentage: customPercentag
+// };
+
+// let tips = document.querySelectorAll(".tip");
+// console.log(tips);
+// tips.forEach((tip)=>{
+//     tip.addEventListener("click", ()=>{
+//         document.querySelector(".salam").classList.remove("salam");
+//         tip.classList.add("salam");
+
+//         let percentage = tip.querySelector(".percentage").textContent;
+//         obj.tipPercentage = percentage;
+//     })
+// })
+
+// sendBtn.addEventListener("click", ()=>{
+//     let bill = document.querySelector(".billInput").value;
+//     let people = document.querySelector(".peopleInput").value;
+//     let priceTip = document.querySelector(".priceTip");
+//     let priceTotal = document.querySelector(".priceTotal");
+
+//     if (obj.customPercentage){
+//         let anotherRes = parseInt(bill * (obj.customPercentage * people)) / 100;
+//         priceTip.textContent = anotherRes + "$";
+//         priceTotal.textContent = parseInt(anotherRes) + parseInt(bill) + "$";    
+//     }
+//     else{
+//     let result = parseInt(bill * (obj.tipPercentage * people)) / 100; 
+//     priceTip.textContent = result + "$";
+//     priceTotal.textContent = parseInt(result) + parseInt(bill) + "$";
+//     }
+// });
+// let sendBtn = document.querySelector(".submit");
+// let customPercentag = document.querySelector(".customPercentage").value;
+// let obj = {
+//     tipPercentage: null,
+//     customPercentage: customPercentag
+// };
+
+// let tips = document.querySelectorAll(".tip");
+// console.log(tips);
+// tips.forEach((tip)=>{
+//     tip.addEventListener("click", ()=>{
+//         document.querySelector(".salam").classList.remove("salam");
+//         tip.classList.add("salam");
+
+//         let percentage = tip.querySelector(".percentage").textContent;
+//         obj.tipPercentage = percentage;
+//     })
+// })
+
+// sendBtn.addEventListener("click", ()=>{
+//     let bill = document.querySelector(".billInput").value;
+//     let people = document.querySelector(".peopleInput").value;
+//     let priceTip = document.querySelector(".priceTip");
+//     let priceTotal = document.querySelector(".priceTotal");
+
+//     if (obj.customPercentage){
+//         let anotherRes = parseInt(bill * (obj.customPercentage * people)) / 100;
+//         priceTip.textContent = anotherRes + "$";
+//         priceTotal.textContent = parseInt(anotherRes) + parseInt(bill) + "$";    
+//     }
+//     else{
+//     let result = parseInt(bill * (obj.tipPercentage * people)) / 100; 
+//     priceTip.textContent = result + "$";
+//     priceTotal.textContent = parseInt(result) + parseInt(bill) + "$";
+//     }
+// });
+// let sendBtn = document.querySelector(".submit");
+// let customPercentag = document.querySelector(".customPercentage").value;
+// let obj = {
+//     tipPercentage: null,
+//     customPercentage: customPercentag
+// };
+
+// let tips = document.querySelectorAll(".tip");
+// console.log(tips);
+// tips.forEach((tip)=>{
+//     tip.addEventListener("click", ()=>{
+//         document.querySelector(".salam").classList.remove("salam");
+//         tip.classList.add("salam");
+
+//         let percentage = tip.querySelector(".percentage").textContent;
+//         obj.tipPercentage = percentage;
+//     })
+// })
+
+// sendBtn.addEventListener("click", ()=>{
+//     let bill = document.querySelector(".billInput").value;
+//     let people = document.querySelector(".peopleInput").value;
+//     let priceTip = document.querySelector(".priceTip");
+//     let priceTotal = document.querySelector(".priceTotal");
+
+//     if (obj.customPercentage){
+//         let anotherRes = parseInt(bill * (obj.customPercentage * people)) / 100;
+//         priceTip.textContent = anotherRes + "$";
+//         priceTotal.textContent = parseInt(anotherRes) + parseInt(bill) + "$";    
+//     }
+//     else{
+//     let result = parseInt(bill * (obj.tipPercentage * people)) / 100; 
+//     priceTip.textContent = result + "$";
+//     priceTotal.textContent = parseInt(result) + parseInt(bill) + "$";
+//     }
+// });
+// let sendBtn = document.querySelector(".submit");
+// let customPercentag = document.querySelector(".customPercentage").value;
+// let obj = {
+//     tipPercentage: null,
+//     customPercentage: customPercentag
+// };
+
+// let tips = document.querySelectorAll(".tip");
+// console.log(tips);
+// tips.forEach((tip)=>{
+//     tip.addEventListener("click", ()=>{
+//         document.querySelector(".salam").classList.remove("salam");
+//         tip.classList.add("salam");
+
+//         let percentage = tip.querySelector(".percentage").textContent;
+//         obj.tipPercentage = percentage;
+//     })
+// })
+
+// sendBtn.addEventListener("click", ()=>{
+//     let bill = document.querySelector(".billInput").value;
+//     let people = document.querySelector(".peopleInput").value;
+//     let priceTip = document.querySelector(".priceTip");
+//     let priceTotal = document.querySelector(".priceTotal");
+
+//     if (obj.customPercentage){
+//         let anotherRes = parseInt(bill * (obj.customPercentage * people)) / 100;
+//         priceTip.textContent = anotherRes + "$";
+//         priceTotal.textContent = parseInt(anotherRes) + parseInt(bill) + "$";    
+//     }
+//     else{
+//     let result = parseInt(bill * (obj.tipPercentage * people)) / 100; 
+//     priceTip.textContent = result + "$";
+//     priceTotal.textContent = parseInt(result) + parseInt(bill) + "$";
+//     }
+// });
+// let sendBtn = document.querySelector(".submit");
+// let customPercentag = document.querySelector(".customPercentage").value;
+// let obj = {
+//     tipPercentage: null,
+//     customPercentage: customPercentag
+// };
+
+// let tips = document.querySelectorAll(".tip");
+// console.log(tips);
+// tips.forEach((tip)=>{
+//     tip.addEventListener("click", ()=>{
+//         document.querySelector(".salam").classList.remove("salam");
